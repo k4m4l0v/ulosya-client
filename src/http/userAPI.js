ed00b53b4f6077eefa6c90749aafd90c4a6f4be8
Даниил <daniil.kamalov2001@mail.ru>
2026-05-22 21:21:03 +0300

    c

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -1,9 +1,9 @@
 import {$host} from './index';
 
-export const getItems = async () => {
-    console.log('getItems called'); // Отладочный лог
-    console.trace(); // Показывает цепочку вызовов
-    
-    const {data} = await $host.get('api/item');
-    return data;
-}
+// export const getItems = async () => {
+//     console.log('getItems called'); // Отладочный лог
+//     console.trace(); // Показывает цепочку вызовов
+
+//     const {data} = await $host.get('api/item');
+//     return data;
+// }
